98cd3a67c5acf91875647b22dc6f2d58a34b3b1d
tuanpham <tuanpha@lungvang.vn>
2016-11-09 18:16:33 +0700

    first commit

---
+++ client/app/modules/index/config/index.routes.js
@@ -0,0 +1,24 @@
+(function () {
+  'use strict';
+  angular
+    .module('com.module.index')
+    .config(function ($stateProvider) {
+      $stateProvider
+        .state('index', {
+          abstract: true,
+          url: '',
+          templateUrl: 'modules/index/views/main.html',
+          controller: 'HomeCtrl'
+        })
+        .state('index.homepage', {
+          url: '/index',
+          templateUrl: 'modules/index/views/homepage.html'
+        })
+        .state('index.price', {
+          url: '/price',
+          templateUrl: 'modules/index/views/price.html',
+          controller: "PriceCtrl"
+        });
+    });
+
+})();
---
+++ client/app/modules/index/controllers/price.ctrl.js
@@ -0,0 +1,9 @@
+(function () {
+  'use strict';
+  angular
+    .module('com.module.index')
+    .controller('PriceCtrl', function ($scope, $rootScope, $state, $location) {
+
+
+    })
+}) ();
