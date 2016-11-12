3f4ab4e9a6ec14e4bebc82e1eb2a87d34f2e33f6
kid1412dn <thiennguyenph133@gmail.com>
2016-11-12 11:10:06 +0700

    we offer and fix header homepage

--- client/app/modules/index/config/index.routes.js
+++ client/app/modules/index/config/index.routes.js
@@ -16,6 +16,11 @@ angular
 	            templateUrl: 'modules/index/views/home_header.html',
 	            controller: 'demo'
 	          },
+
+	          'home_weoffer@home': {
+	            templateUrl: 'modules/index/views/home_weoffer.html',
+	            controller: 'demo',
+	          },
 	          'home_ss2info@home': {
 	            templateUrl: 'modules/index/views/home_ss2info.html',
 	            controller: 'demo'
