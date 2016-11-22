09bf33f527a5f404dff419bf0d537099537a878c
nghiapham0302 <danchoi9x>
2016-11-22 09:14:25 +0700

    change img

--- client/app/modules/index/controllers/index.ctrl.js
+++ client/app/modules/index/controllers/index.ctrl.js
@@ -4,12 +4,12 @@ angular
   .controller('index', function($scope,$modal,$rootScope) {
     $rootScope.step = 0;
     $rootScope.tourId = 1;
-    function OpenModal(cb) {          
+    function OpenModal(cb) {
       $modal.open({
-            animation: false,                               
-            templateUrl: 'modules/index/views/booking-modal/modal-step1.html',
-            controller: 'modal3Step',
-            size: 'lg'
+        animation: false,
+        templateUrl: 'modules/index/views/booking-modal/modal-step1.html',
+        controller: 'modal3Step',
+        size: 'lg'
       });
       cb()
     }
@@ -24,12 +24,12 @@ angular
     //////booking modal data
     $rootScope.step = 0;
     $rootScope.tourId = 1;
-    function OpenModal(cb) {          
+    function OpenModal(cb) {
       $modal.open({
-            animation: false,                               
-            templateUrl: 'modules/index/views/booking-modal/modal-step1.html',
-            controller: 'modal3Step',
-            size: 'lg'
+        animation: false,
+        templateUrl: 'modules/index/views/booking-modal/modal-step1.html',
+        controller: 'modal3Step',
+        size: 'lg'
       });
       cb()
     }
@@ -40,64 +40,64 @@ angular
       });
     };
     //////
-    $scope.ss2showinfos =[ 
-    {
-      ss2title: 'Walking',
-      ss2image: 'images/Walking.jpg',
-      ss2content:'Our signature FOODY STORY TOUR for those who are not afraid to bare the elements and wish to walk off some extra calories from indulging in the 7 sinfully delicious dishes.'
-    },
-    {
-      ss2title: 'Cyclo',
-      ss2image: 'images/Cyclo.jpg',
-      ss2content:'How about a piece of Vietnamese history and some interesting photo ops while enjoying your FOODY STORY TOUR? Cyclo is a three-wheeled bicycle taxi that will bring you back to the French colonial times. You will enjoy the tour in a truly colonial fashion'
-    },
-    {
-      ss2title: 'Taxi',
-      ss2image: 'images/Taxi.jpeg',
-      ss2content:'Have a group of friends or your hotel is far from the city center? FOODY STORY TOUR by taxi offers door-to-door pickup and drop off service for those who desire comfort while enjoying the show.'
-    }
+    $scope.ss2showinfos =[
+      {
+        ss2title: 'Walking',
+        ss2image: 'images/Walking.jpg',
+        ss2content:'Our signature <strong>FOODY STORY TOUR</strong> for those who are not afraid to put some mileage on their flip flops, who wish to walk off some extra calories from indulging in the 7 sinfully delicious dishes.'
+      },
+      {
+        ss2title: 'Cyclo',
+        ss2image: 'images/Cyclo.jpg',
+        ss2content:'How about a piece of Vietnamese history and some interesting photo ops while enjoying your <strong>FOODY STORY TOUR</strong> ? Cyclo is a three-wheeled bicycle taxi that will bring you back to the French colonial times. You will enjoy the tour in a truly colonial fashion'
+      },
+      {
+        ss2title: 'Taxi',
+        ss2image: 'images/Taxi.jpg',
+        ss2content:'Have a group of friends or your hotel is far from the city center? <strong>FOODY STORY TOUR</strong> by taxi offers door-to-door pickup and drop off service for those who desire comfort while enjoying the show.'
+      }
 
     ];
-    
+
   })
   .controller('ss3slides', function($scope) {
-    $scope.slides =[ 
-    {
-      ss3image1: 'images/1.jpg',
-      ss3image2: 'images/2.jpg',
-      ss3image3: 'images/3.jpg'
-    },
-    {
-      ss3image1: 'images/2.jpg',
-      ss3image2: 'images/3.jpg',
-      ss3image3: 'images/4.jpg'
-    },
-    {
-      ss3image1: 'images/3.jpg',
-      ss3image2: 'images/4.jpg',
-      ss3image3: 'images/5.jpg'
-    },
-    {
-      ss3image1: 'images/4.jpg',
-      ss3image2: 'images/5.jpg',
-      ss3image3: 'images/6.jpg'
-    },
-    {
-      ss3image1: 'images/5.jpg',
-      ss3image2: 'images/6.jpg',
-      ss3image3: 'images/7.jpg'
-    },
-    {
-      ss3image1: 'images/6.jpg',
-      ss3image2: 'images/7.jpg',
-      ss3image3: 'images/1.jpg'
-    },
-    {
-      ss3image1: 'images/7.jpg',
-      ss3image2: 'images/1.jpg',
-      ss3image3: 'images/2.jpg' 
-    } 
+    $scope.slides =[
+      {
+        ss3image1: 'images/1.jpg',
+        ss3image2: 'images/2.jpg',
+        ss3image3: 'images/3.jpg'
+      },
+      {
+        ss3image1: 'images/2.jpg',
+        ss3image2: 'images/3.jpg',
+        ss3image3: 'images/4.jpg'
+      },
+      {
+        ss3image1: 'images/3.jpg',
+        ss3image2: 'images/4.jpg',
+        ss3image3: 'images/5.jpg'
+      },
+      {
+        ss3image1: 'images/4.jpg',
+        ss3image2: 'images/5.jpg',
+        ss3image3: 'images/6.jpg'
+      },
+      {
+        ss3image1: 'images/5.jpg',
+        ss3image2: 'images/6.jpg',
+        ss3image3: 'images/7.jpg'
+      },
+      {
+        ss3image1: 'images/6.jpg',
+        ss3image2: 'images/7.jpg',
+        ss3image3: 'images/1.jpg'
+      },
+      {
+        ss3image1: 'images/7.jpg',
+        ss3image2: 'images/1.jpg',
+        ss3image3: 'images/2.jpg'
+      }
     ];
-    
+
   })
 
